ad78936dd1c45ba8e9c347ba397b42a1842c92cf
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-02-08 20:34:54 +0200

    Implemented MongoDB connection setup and initialization of Gin router for handling HTTP requests.

---
+++ cmd/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/AhmedFatthy1040/OrganizationHub-API/pkg/api/routes"
+	"github.com/AhmedFatthy1040/OrganizationHub-API/pkg/database/mongodb/repository"
+)
+
+var (
+	client     *mongo.Client
+	database   *mongo.Database
+	usersCollection     *mongo.Collection
+	organizationsCollection *mongo.Collection
+	tokensCollection *mongo.Collection
+	invitationsCollection *mongo.Collection
+)
+
+func init() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// Get MongoDB URI from environment variables
+	mongoURI := os.Getenv("MONGODB_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI not found in .env file")
+	}
+
+	// Connect to MongoDB
+	client, err = mongo.Connect(context.Background(), options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatalf("Error connecting to MongoDB: %v", err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		log.Fatalf("Error pinging MongoDB: %v", err)
+	}
+	log.Println("Connected to MongoDB!")
+
+	// Set database
+	databaseName := os.Getenv("DATABASE_NAME")
+	database = client.Database(databaseName)
+
+	// Set collections
+	usersCollectionName := os.Getenv("USERS_COLLECTION")
+	organizationsCollectionName := os.Getenv("ORGANIZATIONS_COLLECTION")
+	tokensCollectionName := os.Getenv("TOKENS_COLLECTION")
+	invitationsCollectionName := os.Getenv("INVITATIONS_COLLECTION")
+
+	usersCollection = database.Collection(usersCollectionName)
+	organizationsCollection = database.Collection(organizationsCollectionName)
+	tokensCollection = database.Collection(tokensCollectionName)
+	invitationsCollection = database.Collection(invitationsCollectionName)
+}
+
+func main() {
+    // Initialize Gin router
+	router := gin.Default()
+
+	// Initialize user repository
+    userRepository := repository.NewUserRepository(database)
+
+    // Setup user routes
+    routes.SetupUserRoutes(router, userRepository)
+
+	// Start the Gin server
+	router.Run(":8080")
+}
