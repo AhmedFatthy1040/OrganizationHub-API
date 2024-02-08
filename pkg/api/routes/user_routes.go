823c53065715ecb89027ca5319b8dc52afb20361
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-02-08 20:38:29 +0200

    Defined routes for user-related endpoints (signup and signin).

---
+++ pkg/api/routes/user_routes.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/AhmedFatthy1040/OrganizationHub-API/pkg/api/handlers"
+	"github.com/AhmedFatthy1040/OrganizationHub-API/pkg/database/mongodb/repository"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupUserRoutes(router *gin.Engine, userRepository *repository.UserRepository) {
+    // Initialize user handler
+    userHandler := handlers.NewUserHandler(userRepository)
+
+    // Define user-related routes
+    userRoutes := router.Group("/users")
+    {
+        userRoutes.POST("/signup", userHandler.Signup)
+        userRoutes.POST("/signin", userHandler.Signin)
+    }
+}
