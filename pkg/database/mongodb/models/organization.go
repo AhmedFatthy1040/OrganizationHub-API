29af5289afb48b33201992a79dc234836fa094ab
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-02-08 20:36:20 +0200

    Added Organization struct definition to represent organizations.

---
+++ pkg/database/mongodb/models/organization.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Organization struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
+	Description string             `json:"description,omitempty" bson:"description,omitempty"`
+	CreatedAt   time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt   time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+}
