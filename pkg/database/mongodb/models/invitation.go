7716a128860b3167623cfd8eceeaf71748f4a925
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-02-09 20:23:08 +0200

    Updated the Invitation model in the models package.

--- pkg/database/mongodb/models/invitation.go
+++ pkg/database/mongodb/models/invitation.go
@@ -9,8 +9,7 @@ import (
 type Invitation struct {
 	ID              primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	OrganizationID  primitive.ObjectID `json:"organization_id,omitempty" bson:"organization_id,omitempty"`
-	InvitedEmail    string             `json:"invited_email,omitempty" bson:"invited_email,omitempty"`
+	InvitedEmail    string             `json:"user_email,omitempty" bson:"invited_email,omitempty"`
 	InvitationToken string             `json:"invitation_token,omitempty" bson:"invitation_token,omitempty"`
-	ExpiresAt       time.Time          `json:"expires_at,omitempty" bson:"expires_at,omitempty"`
 	CreatedAt       time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
 }
