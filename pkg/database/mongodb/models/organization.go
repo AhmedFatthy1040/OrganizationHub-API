e31b40e942c281516ce358d4e8c4795ca3914904
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-02-09 20:23:36 +0200

    Updated the Organization and OrganizationMember models in the models package.

--- pkg/database/mongodb/models/organization.go
+++ pkg/database/mongodb/models/organization.go
@@ -6,10 +6,17 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type OrganizationMember struct {
+	Name        string `json:"name,omitempty" bson:"name,omitempty"`
+	Email       string `json:"email,omitempty" bson:"email,omitempty"`
+	AccessLevel string `json:"access_level,omitempty" bson:"access_level,omitempty"`
+}
+
 type Organization struct {
-	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name        string             `json:"name,omitempty" bson:"name,omitempty"`
-	Description string             `json:"description,omitempty" bson:"description,omitempty"`
-	CreatedAt   time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
-	UpdatedAt   time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	ID           primitive.ObjectID   `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name         string               `json:"name,omitempty" bson:"name,omitempty"`
+	Description  string               `json:"description,omitempty" bson:"description,omitempty"`
+	CreatedAt    time.Time            `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt    time.Time            `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
+	Members      []OrganizationMember `json:"members,omitempty" bson:"members,omitempty"`
 }
