59eb7e38c9b29b6b90c17abbc08de500cd926b5e
AhmedFatthy1040 <91102592+AhmedFatthy1040@users.noreply.github.com>
2024-02-09 20:24:40 +0200

    Updated the User model in the models package.

--- pkg/database/mongodb/models/user.go
+++ pkg/database/mongodb/models/user.go
@@ -11,6 +11,8 @@ type User struct {
     Name      string             `json:"name,omitempty" bson:"name,omitempty"`
     Email     string             `json:"email,omitempty" bson:"email,omitempty"`
     Password  string             `json:"password,omitempty" bson:"password,omitempty"`
+    AccessToken  string             `json:"access_token,omitempty" bson:"access_token,omitempty"`
+	RefreshToken string             `json:"refresh_token,omitempty" bson:"refresh_token,omitempty"`
     CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
     UpdatedAt time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
